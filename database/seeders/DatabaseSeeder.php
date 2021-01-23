ea465e155c2b6825e5d938e95e64b47c6c404d43
abdullahalharun <harun750@gmail.com>
2021-01-23 12:35:49 +0600

    update db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name'      => 'Tti Desk',
+            'email'     => 'ttidesk.bd@gmail.com',
+            'password'  => 'ttideskbd321',
+
+        ]);
     }
 }
