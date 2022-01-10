f7452079e4223cf5bce1c1796853c4f61bf9ae3d
abdullahalharun <harun750@gmail.com>
2022-01-10 13:07:41 +0600

    update salary

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">            
               
                 <div class="flex flex-wrap mx-4 my-8">
-                    <div class="w-1/2 xl:w-1/4 px-3">
+                    <div class="w-1/2 xl:w-1/3 px-3">
                         <div class="w-full bg-white border text-red-400 rounded-lg flex items-center p-6 mb-6 xl:mb-0">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12 fill-current mr-4 hidden lg:block" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M16.707 10.293a1 1 0 010 1.414l-6 6a1 1 0 01-1.414 0l-6-6a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l4.293-4.293a1 1 0 011.414 0z" clip-rule="evenodd" />
@@ -21,7 +21,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="w-1/2 xl:w-1/4 px-3">
+                    <div class="w-1/2 xl:w-1/3 px-3">
                         <div class="w-full bg-white border text-green-400 rounded-lg flex items-center p-6 mb-6 xl:mb-0">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12 fill-current mr-4 hidden lg:block" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M3.293 9.707a1 1 0 010-1.414l6-6a1 1 0 011.414 0l6 6a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L4.707 9.707a1 1 0 01-1.414 0z" clip-rule="evenodd" />
@@ -36,7 +36,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="w-1/2 xl:w-1/4 px-3">
+                    <div class="w-1/2 xl:w-1/3 px-3">
                         <div class="w-full bg-white border text-green-400 rounded-lg flex items-center p-6">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12 fill-current mr-4 hidden lg:block" viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z" />
@@ -47,7 +47,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="w-1/2 xl:w-1/4 px-3">
+                    <!-- <div class="w-1/2 xl:w-1/4 px-3">
                         <div class="w-full bg-white border text-blue-400 rounded-lg flex items-center p-6"><svg viewBox="0 0 20 20" class="w-16 h-16 fill-current mr-4 hidden lg:block">
                                 <path d="M17.431,2.156h-3.715c-0.228,0-0.413,0.186-0.413,0.413v6.973h-2.89V6.687c0-0.229-0.186-0.413-0.413-0.413H6.285c-0.228,0-0.413,0.184-0.413,0.413v6.388H2.569c-0.227,0-0.413,0.187-0.413,0.413v3.942c0,0.228,0.186,0.413,0.413,0.413h14.862c0.228,0,0.413-0.186,0.413-0.413V2.569C17.844,2.342,17.658,2.156,17.431,2.156 M5.872,17.019h-2.89v-3.117h2.89V17.019zM9.587,17.019h-2.89V7.1h2.89V17.019z M13.303,17.019h-2.89v-6.651h2.89V17.019z M17.019,17.019h-2.891V2.982h2.891V17.019z"></path>
                             </svg>
@@ -56,7 +56,7 @@
                                 <p>Last Month</p>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
                           
             </div>
