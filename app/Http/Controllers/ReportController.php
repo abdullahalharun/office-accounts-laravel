6d98e9e3552c3e30127deff5ba269be239dee997
abdullahalharun <harun750@gmail.com>
2022-12-18 18:35:29 +0600

    update monthly report print option

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -62,7 +62,7 @@ class ReportController extends Controller
     public function monthly_print_format(Request $request)
     {
         $expenses = Expense::groupBy('category_id')
-            ->selectRaw('id, date, category_id, account_id, amount, sum(amount) as total_amount, charge, sum(charge) as total_charge')
+            ->selectRaw('id, date, category_id, account_id, details, amount, sum(amount) as total_amount, charge, sum(charge) as total_charge')
             ->whereBetween('date', [$request->datefrom, $request->dateto])
             ->get();
 
